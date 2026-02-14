229e63b01013f104e19fbf776e2b708a0edc8d0f
Najaf Shaikh <najafshaikh1705@gmail.com>
2026-02-14 17:26:11 +0530

    fixed backend bug

--- server/src/main/java/com/arch/server/models/Graph/GraphNode.java
+++ server/src/main/java/com/arch/server/models/Graph/GraphNode.java
@@ -1,10 +1,12 @@
 package com.arch.server.models.Graph;
 
+import java.util.Map;
+
 public class GraphNode {
 
     private String id;
     private String type;
 
-    private Position position;
+    private NodePosition position;
     private Map<String, Object> data;
 }
